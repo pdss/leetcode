3e1cf29382b60360f47716460a042664366027c0
luotao <307958201@qq.com>
2019-09-21 12:16:12 +0800

    Initial commit

---
+++ src/main/java/com/st/shandiao/ShandiaoApplication.java
@@ -0,0 +1,13 @@
+package com.st.shandiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShandiaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShandiaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/st/shandiao/ShandiaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.st.shandiao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShandiaoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
